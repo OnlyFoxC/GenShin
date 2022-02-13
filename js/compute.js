a0abd55b47caa2918a81bf1ec7c21809ae16d9a8
OnlyFoxC <99579719+OnlyFoxC@users.noreply.github.com>
2022-02-13 14:34:53 +0800

    Add files via upload

---
+++ js/compute.js
@@ -0,0 +1,302 @@
+function wish(num) {//num=抽卡次数
+	var selectCard=document.getElementsByClassName('select');
+	var id=selectCard[0].id;
+	var sort;
+	
+	var UP5=new Array;
+	var UP4=new Array;
+	for(var d in data){//判断卡池类型
+		if(data[d].版本号+data[d].名称==id){
+			sort=data[d].类型;
+			UP5=strToArray(data[d].五星UP);
+			UP4=strToArray(data[d].四星UP);
+			break;
+		}
+	}
+	//常驻5星物品
+	var role5=new Array;
+	var weapons5=new Array;
+	for(var i in 祈愿物品.五星物品){
+		if(i>=42&&i<=46){
+			role5.push(祈愿物品.五星物品[i].常驻角色.replace('；',''));
+		}
+		if(i>=16&&i<=25){
+			weapons5.push(祈愿物品.五星物品[i].常驻武器.replace('；',''));
+		}
+	}
+	//4星物品
+	var role4=new Array;
+	var weapons4=new Array;
+	for(var i in 祈愿物品.四星物品){
+		if(i>=27&&i<=50){
+			role4.push(祈愿物品.四星物品[i].角色.replace('；',''));
+		}
+		if(i>=0&&i<=26){
+			weapons4.push(祈愿物品.四星物品[i].武器.replace('；',''));
+		}
+	}
+	var str3=new Array;
+	for(var i in 祈愿物品.三星物品){
+		str3.push(祈愿物品.三星物品[i].武器.replace('；',''));
+	}
+	var endResults=new Array;//存储当前抽取的所有结果
+	for(var i=0;i<num;i++){//根据num次数去抽取结果
+		var name=randomTh(sort,str3,UP4,UP5,role4,role5,weapons4,weapons5);//单次抽取结果
+		endResults.push(name);
+        writeLocalStorage(name);//写入历史记录
+	}
+	var n;
+	for(var i in endResults){
+		if(endResults[i].indexOf('5')!=-1){
+			n=5;
+			break;
+		}else if(endResults[i].indexOf('4')!=-1){
+			n=4;
+			break;
+		}else{
+			n=3;
+		}
+	}
+	videoPS(n);
+	showResults(endResults);
+}
+function randomTh(sort,str3,UP4,UP5,role4,role5,weapons4,weapons5){//随机选择物品
+	var name;
+	var str5=new Array;
+	var str4=new Array;
+	if(sort=='常驻'){
+		str5=role5.concat(weapons5);
+		str4=role4.concat(weapons4);
+		UP5=str5;
+		UP4=str4;
+	}else if(sort=='武器'){
+		str5=weapons5.concat(UP5);
+		str4=weapons4;
+	}else{
+		str5=role5.concat(UP5);
+		str4=role4;
+	}
+	
+	var isE=isEnsure();//保底判断
+	if(isE==15){
+		name=UP5[Math.round(Math.random()*(UP5.length-1))]+'U5';
+	}else if(isE==5){
+		var n=Math.round(Math.random());//0-1
+		if(n==0){
+			name=str5[Math.round(Math.random()*(str5.length-1))];
+		}else{
+			name=UP5[Math.round(Math.random()*(UP5.length-1))];
+		}
+		for(var i in UP5){
+			if(UP5[i].indexOf(name)!=-1){
+				name+='U5';
+				break;
+			}else{
+				name+='5';
+				break;
+			}
+		}
+	}else if(isE==14){
+		name=UP4[Math.round(Math.random()*(UP4.length-1))]+'U4';
+	}else if(isE==4){
+		var num=Math.round(Math.random()*(1000-1)+1);
+		if(num<=6){
+			var n=Math.round(Math.random());//0-1
+			if(n==0){
+				name=str5[Math.round(Math.random()*(str5.length-1))];
+			}else{
+				name=UP5[Math.round(Math.random()*(UP5.length-1))];
+			}
+			for(var i in UP5){
+				if(UP5[i].indexOf(name)!=-1){
+					name+='U5';
+					break;
+				}else{
+					name+='5';
+					break;
+				}
+			}
+			return name;
+		}
+		var n=Math.round(Math.random());//0-1
+		if(n==0){
+			name=str4[Math.round(Math.random()*(str4.length-1))];
+			for(var i in UP4){
+				if(UP4[i].indexOf(name)!=-1){
+					name+='U4';
+					break;
+				}else{
+					name+='4';
+					break;
+				}
+			}
+		}else{
+			name=UP4[Math.round(Math.random()*(UP4.length-1))]+'U4';
+		}
+	}else{
+		var starN=star(sort);
+		if(starN==5){
+			name=str5[Math.round(Math.random()*(str5.length-1))];
+			for(var i in UP5){
+				if(UP5[i].indexOf(name)!=-1){
+					name+='U5';
+					break;
+				}else{
+					name+='5';
+					break;
+				}
+			}
+		}else if(starN==4){
+			name=str4[Math.round(Math.random()*(str4.length-1))];
+			for(var i in UP5){
+				if(UP4[i].indexOf(name)!=-1){
+					name+='U4';
+					break;
+				}else{
+					name+='4';
+					break;
+				}
+			}
+		}else{
+			name=str3[Math.round(Math.random()*(str3.length-1))]+'3';
+		}
+	}
+	return name;
+}
+
+function isEnsure(){//保底
+	var historys=readLocalStorage();
+	//  10次4星  20次4星   90次5星  180次5星
+	var small4=1,great4=1,small5=1,great5=1;
+	if(historys.length<9){small4=0;}
+	if(historys.length<19){great4=0;}
+	if(historys.length<89){small5=0;}
+	if(historys.length<179){great5=0;}
+	for(var i=historys.length-1,n=0;i>=0;i--,n++){//倒序读取历史记录
+		if(historys[i].indexOf('4')!=-1&&n<9){
+			small4=0;
+		}
+		if(historys[i].indexOf('U4')!=-1&&n<19){
+			great4=0;
+		}
+		if(historys[i].indexOf('5')!=-1&&n<89){
+			small5=0;
+		}
+		if(historys[i].indexOf('U5')!=-1&&n<179){
+			great5=0; 
+		}
+		if(n>180){break;}
+	}
+	if(great5==1){
+		return 15;
+	}else if(small5==1){
+		return 5;
+	}else if(great4==1){
+		return 14;
+	}else if(small4==1){
+		return 4;
+	}
+	return 0;
+}
+function writeLocalStorage(name){//存储历史记录到本地
+	if(!window.localStorage){
+		alert("浏览器不支持当前历史记录存储方式（localstorage），可以更换高版本浏览器试试！");
+		return;
+	}else{
+		var storage=window.localStorage;
+		storage['历史记录']+=name+',';
+	}
+}
+function readLocalStorage(){//读取本地所有的历史记录
+	var historys=new Array;
+	if(!window.localStorage){
+		alert("浏览器不支持当前历史记录存储方式（localstorage），可以更换高版本浏览器试试！");
+		return;
+	}else{
+		var storage=window.localStorage;
+		if(storage['历史记录']==undefined){
+			storage['历史记录']='';
+		}
+		var str=storage['历史记录'];
+		var name='';//存储临时的名字
+		for(var i=0;i<str.length;i++){
+			if(str[i]!=','){
+				name+=str[i];
+			}else{
+				historys.push(name);
+				name='';
+			}
+		}
+	}
+	return historys;
+}
+function jsonToAry(j){
+	var ary=new Array;
+	for(var i=0;i<j.length;i++){
+		for(var v in j[i]){
+			ary.push(j[i][v].replace('；',''));
+		}
+	}
+	return ary;
+}
+function strToArray(str){
+	var ary=new Array;
+	var name='';//存储临时的名字
+	for(var i=0;i<str.length;i++){
+		if(str[i]!='；'){
+			name+=str[i];
+		}else{
+			ary.push(name);
+			name='';
+		}
+	}
+	return ary;
+}
+
+function videoPS(n,endResults){
+	var video=document.getElementById('video');
+	var videoDiv=document.getElementById('videoDiv');
+	video.setAttribute('src','video/'+n+'star.mp4');
+	video.play();
+	videoDiv.style.display='block';
+	var outVideo=document.getElementById('outVideo');
+	outVideo.style.display='block';
+	var bg=document.getElementById('bg');
+	bg.style.display='none';
+	setTimeout(function(){
+		videoDiv.style.display='none';
+		bg.style.display='block';
+		outVideo.style.display='none';
+	},8000);
+}
+function outV(){
+	var video=document.getElementById('video');
+	var videoDiv=document.getElementById('videoDiv');
+	video.pause();
+	videoDiv.style.display='none';
+	document.getElementById('outVideo').style.display='none';
+	document.getElementById('bg').style.display='block';
+}
+
+//***************************************
+function star(sort){//随机抽取几星
+	//var num=Math.round(Math.random()*(y-x)+x);	//2.550% 基础4星武器
+	var num;
+	if(sort!='武器'){
+		num=Math.round(Math.random()*(1000-1)+1);
+		if(num<=6){
+			return 5;
+		}else if(num<=57){
+			return 4;
+		}
+	}else{//角色与常驻
+		num=Math.round(Math.random()*(1000-1)+1);
+		if(num<=7){
+			return 5;
+		}else if(num<=67){
+			return 4;
+		}
+	}
+	return 3;
+}
+//***************************************
---
+++ js/index.js
@@ -0,0 +1,217 @@
+function startHtml(){
+	//随机图标
+	var iconUrlArray=new Array("fire","grass","ice","rock","thunder","water","wind");
+	var x=Math.floor((Math.random()*7)+1);
+	var iconUrl='img/Icon/'+iconUrlArray[x-1]+'.webp';
+	var link=document.createElement('link');
+	link.href=iconUrl;
+	link.rel='shortcut icon';
+	document.head.appendChild(link);
+	document.getElementById('editionDiv').style.backgroundImage='url("'+iconUrl+'")';
+	loadEdition();
+	changeEdition();
+	for(var i=0;i<data.length;i++){
+		if(data[i].版本号==editionList.value&&data[i].类型=='角色'){
+			editionDiv_p.innerHTML=data[i].名称;
+			changeCard(data[i].版本号+data[i].名称);
+			break;
+		}
+	}
+}
+function closeWin(){
+	open(location, '_self').close();
+}
+function loadEdition(){
+	var editionArray=new Array;
+	for(var i=0;i<data.length;i++){
+		if(i==0){
+			editionArray.push(data[i].版本号);
+		}else{
+			if(data[i].版本号!=data[i-1].版本号){
+				editionArray.push(data[i].版本号);
+			}
+		}
+	}
+	for(var i=0;i<editionArray.length;i++){
+		document.getElementById('editionList')
+		.options.add(new Option(editionArray[i]));
+	}
+}
+function changeEdition(){
+	var editionList=document.getElementById('editionList');
+	var editionDiv_p=document.getElementById('editionDiv_p');
+	var headList=document.getElementById('headList');
+	//删除 li 控件  BUG:for循环，删不干净
+	var hL=headList.children.length;
+	while(hL>0){
+		headList.children[hL-1].remove();
+		hL--;
+	}
+	
+	for(var i=0;i<data.length;i++){
+		if(data[i].版本号==editionList.value){
+			var li=document.createElement('li');
+			li.id=data[i].版本号+data[i].名称;
+			li.setAttribute('onclick','changeCard(id)');
+			headList.appendChild(li);
+		}
+	}
+	var permanentli=document.createElement('li');
+	permanentli.id='常驻';
+	permanentli.className='select';
+	//给 li 控件添加点击事件
+	permanentli.setAttribute('onclick','changeCard(id)');
+	headList.appendChild(permanentli);
+	
+	if(headList.children.length>3){
+		document.getElementById('headList').style.width='250px';
+	}else{
+		document.getElementById('headList').style.width='200px';
+	}
+	
+	for(var i=0;i<data.length;i++){
+		if(data[i].版本号==editionList.value&&data[i].类型=='角色'){
+			editionDiv_p.innerHTML=data[i].名称;
+			changeCard(data[i].版本号+data[i].名称);
+			break;
+		}
+	}
+}
+function changeCard(id){
+	var headList=document.getElementById('headList');
+	for(var i=0;i<headList.children.length;i++){
+		headList.children[i].className='';
+		headList.children[i].style.backgroundImage='url(./img/未选中.png)';
+		headList.children[i].style.width='49px';
+		headList.children[i].style.height='19px';
+		headList.children[i].style.margin=' 0 6px';
+		headList.children[i].style.transform='translate(-17px,0)';
+	}
+
+	var selectLi=document.getElementById(id);
+	selectLi.className='select';
+	
+	var li=document.getElementsByClassName('select');
+	li[0].style.backgroundImage='url(./img/选中.png)';
+	li[0].style.width='58px';
+	li[0].style.height='23px';
+	li[0].style.margin='0 3px';
+	li[0].style.transform='translate(-17px,-1px)';
+	var imgUrl='./img/card/'+li[0].id+'.jpg';
+	var card=document.getElementById('card');
+	card.style.backgroundImage='url('+imgUrl+')';
+}
+function test(){
+	var close=document.getElementById('close');
+	close.onclick='changeCard("1.0（上半）杯装之诗")';
+}
+function LRBtn(d){
+	var headList=document.getElementById('headList');
+	var n;
+	for(var i=0;i<headList.children.length;i++){
+		if(headList.children[i].className=='select'){
+			n=i;
+			headList.children[i].className='';
+		}
+	}
+	var index=n+d;
+	if(index>headList.children.length-1){
+		index=0;
+	}else if(index<0){
+		index=headList.children.length-1;
+	}
+	headList.children[index].className='select';
+	changeCard(headList.children[index].id);
+}
+function historys(){
+	document.getElementById('historysDiv').style.display='block';
+	var historysTable=document.getElementById('historysTable');
+	var hL=historysTable.children.length;
+	while(hL>0){
+		historysTable.children[hL-1].remove();
+		hL--;
+	}
+	var historysAry=readLocalStorage();
+	for(var i=historysAry.length-1;i>=0;i--){
+		historysAry[i].replace('U','');
+		if(historysAry[i].indexOf('5')!=-1){
+			var tr=document.createElement('tr');
+			historysAry[i].replace('5','');
+			tr.innerHTML='<td>'+(i+1)+'</td><td style="color: gold;">'+historysAry[i]+'</td>';
+			historysTable.appendChild(tr);
+		}
+		if(historysAry[i].indexOf('4')!=-1){
+			var tr=document.createElement('tr');
+			historysAry[i].replace('5','');
+			tr.innerHTML='<td>'+(i+1)+'</td><td style="color:#ff69f8;">'+historysAry[i]+'</td>';
+			historysTable.appendChild(tr);
+		}
+		if(historysAry[i].indexOf('3')!=-1){
+			var tr=document.createElement('tr');
+			historysAry[i].replace('3','');
+			tr.innerHTML='<td>'+(i+1)+'</td><td style="color:#3ebcff;">'+historysAry[i]+'</td>';
+			historysTable.appendChild(tr);
+		}
+	}
+}
+function showHistorys(){
+	document.getElementById('historysDiv').style.display='block';
+}
+function historysBack(){
+	document.getElementById('historysDiv').style.display='none';
+}
+function clearHistorys(){
+	var isClear=confirm('真的要删除吗？');
+	if(isClear){
+		var storage=window.localStorage;
+		storage['历史记录']='';
+	}
+	location.reload();
+}
+function closeSmallCard(){
+	var results=document.getElementById('results');
+	results.style.display='none';
+	var smallCardList=document.getElementById('smallCardList');
+	var hL=smallCardList.children.length;
+	while(hL>0){
+		smallCardList.children[hL-1].remove();
+		hL--;
+	}
+}
+function showResults(endResults){
+	var results=document.getElementById('results');
+	results.style.display='block';
+	var ary5=new Array;
+	var ary4=new Array;
+	var ary3=new Array;
+	for(var i in endResults){
+		if(endResults[i].indexOf('5')!=-1||endResults[i].indexOf('U5')!=-1){
+			ary5.push(endResults[i]);
+		}else if(endResults[i].indexOf('4')!=-1||endResults[i].indexOf('U4')!=-1){
+			ary4.push(endResults[i]);
+		}else{
+			ary3.push(endResults[i]);
+		}
+	}
+	// alert(ary5+ary4+ary3);
+	var smallCardList=document.getElementById('smallCardList');
+	
+	for(var i in ary5){
+		var li=document.createElement('li');
+		li.style.backgroundImage='url(./img/smallCard5.png)';
+		li.innerHTML='<img src="img/items/'+ary5[i].replace('U','')+'.png">';
+		smallCardList.appendChild(li);
+	}
+	for(var i in ary4){
+		var li=document.createElement('li');
+		li.style.backgroundImage='url(./img/smallCard4.png)';
+		li.innerHTML='<img src="img/items/'+ary4[i].replace('U','')+'.png">';
+		smallCardList.appendChild(li);
+	}
+	for(var i in ary3){
+		var li=document.createElement('li');
+		li.style.backgroundImage='url(./img/smallCard3.png)';
+		li.innerHTML='<img src="img/items/'+ary3[i]+'.png">';
+		smallCardList.appendChild(li);
+	}
+}
